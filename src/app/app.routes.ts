33e25ae37fc27e2f304fe904022ba7edf8b40d1e
Pelmen1n <59341494+Pelmen1n@users.noreply.github.com>
2026-04-29 02:10:53 +0500

    Add route for upload page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { AuthPage } from './pages/auth/auth';
 import { DashboardPage } from './pages/dashboard/dashboard';
+import { UploadPage } from './pages/upload/upload';   // upload page
 
 export const routes: Routes = [
   { path: '', redirectTo: 'auth', pathMatch: 'full' },
   { path: 'auth', component: AuthPage },
   { path: 'dashboard', component: DashboardPage },
+  { path: 'upload', component: UploadPage },
   { path: '**', redirectTo: 'auth' },
 ];
